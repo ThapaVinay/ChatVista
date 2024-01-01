02572a8e70404678daae7ee897aa698cd6902161
ThapaVinay <thechampthapa@gmail.com>
2024-01-01 20:40:14 +0530

    changes for deploying backend

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-vista-vt.vercel.app/",
   },
 });
 
